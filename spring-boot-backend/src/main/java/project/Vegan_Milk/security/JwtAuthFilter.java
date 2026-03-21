c03d6df965761a34697e8820bd48cfbe1f6fc5e0
Nekster <nekster187@gmail.com>
2026-03-21 13:14:08 +0200

    jwt sercive

--- spring-boot-backend/src/main/java/project/Vegan_Milk/security/JwtAuthFilter.java
+++ spring-boot-backend/src/main/java/project/Vegan_Milk/security/JwtAuthFilter.java
@@ -1,4 +1,20 @@
 package project.Vegan_Milk.security;
 
-public class JwtAuthFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(@NotNull HttpServletRequest request,@NotNull HttpServletResponse response,@NotNull FilterChain filterChain) throws ServletException, IOException {
+
+    }
 }
--- spring-boot-backend/src/main/java/project/Vegan_Milk/security/JwtService.java
+++ spring-boot-backend/src/main/java/project/Vegan_Milk/security/JwtService.java
@@ -1,4 +1,57 @@
 package project.Vegan_Milk.security;
 
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Service
 public class JwtService {
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.expiration}")
+    private long expiration;
+    private SecretKey signKey;
+
+    private Key getSignKey(){
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+    public String generateToken(String email){
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expiration);
+
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(now)
+                .expiration(expiryDate)
+                .signWith(signKey)
+                .compact();
+    }
+    public String extractEmail(String token){
+        return Jwts.parser()
+                .verifyWith(signKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+        public boolean isValid(String token) {
+            try {
+            Jwts.parser()
+                    .verifyWith(signKey)
+                    .build()
+                    .parseSignedClaims(token);
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        }
 }
